765e1cf624b686ef1d57848bae59d98401f1b018
hansemannn <h.knoechel@lambus.com>
2021-02-22 21:07:12 +0100

    chore: finish project

--- android/src/huawei/messaging/MessagingService.kt
+++ android/src/huawei/messaging/MessagingService.kt
@@ -1,14 +1,32 @@
 package huawei.messaging
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.media.RingtoneManager
+import android.os.Build
+import android.preference.PreferenceManager
+import androidx.core.app.NotificationCompat
 import com.huawei.hms.push.HmsMessageService
 import com.huawei.hms.push.RemoteMessage
+import org.appcelerator.kroll.KrollDict
 import org.appcelerator.kroll.common.Log
-import java.util.Arrays
+import org.appcelerator.titanium.TiApplication
+import org.appcelerator.titanium.util.TiConvert
+import org.json.JSONObject
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
 class MessagingService : HmsMessageService() {
 
     override fun onMessageReceived(message: RemoteMessage?) {
         super.onMessageReceived(message)
+        var isVisible: Boolean = true
+        val appInForeground = TiApplication.isCurrentActivityInForeground()
+        val msg = HashMap<String, Any>()
 
         Log.i("HCM", "onMessageReceived is called")
 
@@ -17,6 +35,38 @@ class MessagingService : HmsMessageService() {
             return
         }
 
+        logMessage(message)
+
+        if (message.getNotification() != null) {
+            android.util.Log.d("HCM", "Message Notification Body: " + message.getNotification().getBody())
+            msg.put("title", message.getNotification().getTitle())
+            msg.put("body", message.getNotification().getBody())
+            isVisible = true
+        } else {
+            android.util.Log.d("HCM", "Data message: " + message.getData())
+        }
+
+        msg.put("from", message.getFrom())
+        msg.put("to", message.getTo())
+        msg.put("ttl", message.getTtl())
+        msg.put("messageId", message.getMessageId())
+        msg.put("messageType", message.getMessageType())
+        msg.put("data", KrollDict(message.getData() as MutableMap<out String, out Any>))
+        msg.put("sendTime", message.getSentTime())
+
+        // data message
+        if (!message.data.isEmpty()) {
+            isVisible = showNotification(message)
+        }
+
+        if (isVisible || appInForeground) {
+            TitaniumHuaweiMessagingModule.getInstance().let { module ->
+                module?.onMessageReceived(msg)
+            }
+        }
+    }
+
+    private fun logMessage(message: RemoteMessage) {
         Log.i("HCM", "getCollapseKey: " + message.collapseKey
                 .toString() + "\n getData: " + message.data
                 .toString() + "\n getFrom: " + message.from
@@ -44,10 +94,6 @@ class MessagingService : HmsMessageService() {
              getChannelId: ${notification.channelId}
              getLink: ${notification.link}
              getNotifyId: ${notification.notifyId}""")
-
-        TitaniumHuaweiMessagingModule.getInstance().let { module ->
-            module?.onMessageReceived(message)
-        }
     }
 
     override fun onNewToken(token: String) {
@@ -56,5 +102,129 @@ class MessagingService : HmsMessageService() {
         TitaniumHuaweiMessagingModule.getInstance().let { module ->
             module?.onTokenRefresh(token)
         }
+    }
+
+    private fun showNotification(remoteMessage: RemoteMessage): Boolean {
+        val module = TitaniumHuaweiMessagingModule.getInstance()
+        val params: Map<String?, String?>? = remoteMessage.dataOfMap
+        val jsonData = JSONObject(params)
+        val appInForeground = TiApplication.isCurrentActivityInForeground()
+        var showNotification = true
+        val context = applicationContext
+        var defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        var builderDefaults = 0 or Notification.DEFAULT_SOUND
+        if (appInForeground) {
+            showNotification = false
+        }
+        if (params!!["force_show_in_foreground"] != null && params["force_show_in_foreground"] !== "") {
+            showNotification = showNotification || TiConvert.toBoolean(params["force_show_in_foreground"], false)
+        }
+        if (module != null && module.forceShowInForeground()) {
+            showNotification = module.forceShowInForeground()
+        }
+        if (TiConvert.toBoolean(params["vibrate"], false)) {
+            builderDefaults = builderDefaults or Notification.DEFAULT_VIBRATE
+        }
+        if (params["title"] == null && params["alert"] == null && params["message"] == null && params["big_text"] == null && params["big_text_summary"] == null && params["ticker"] == null && params["image"] == null) {
+            // no actual content - don't show it
+            showNotification = false
+        }
+        val priorityString = params["priority"]
+        var priority = NotificationManager.IMPORTANCE_MAX
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && priorityString != null && !priorityString.isEmpty()) {
+            priority = if (priorityString.toLowerCase() == "low") {
+                NotificationManager.IMPORTANCE_LOW
+            } else if (priorityString.toLowerCase() == "min") {
+                NotificationManager.IMPORTANCE_MIN
+            } else if (priorityString.toLowerCase() == "max") {
+                NotificationManager.IMPORTANCE_MAX
+            } else if (priorityString.toLowerCase() == "default") {
+                NotificationManager.IMPORTANCE_DEFAULT
+            } else if (priorityString.toLowerCase() == "high") {
+                NotificationManager.IMPORTANCE_HIGH
+            } else {
+                TiConvert.toInt(priorityString, 1)
+            }
+        }
+
+        val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = preferences.edit()
+        editor.putString("huawei.messaging.prefs.message", jsonData.toString())
+        editor.commit()
+        if (!showNotification) {
+            // hidden notification - still send broadcast with data for next app start
+            val i = Intent()
+            i.addCategory(Intent.CATEGORY_LAUNCHER)
+            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            i.putExtra("hcm_data", jsonData.toString())
+            sendBroadcast(i)
+
+            return false
+        }
+        val notificationIntent = Intent(this, PushHandlerActivity::class.java)
+        notificationIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        notificationIntent.putExtra("hcm_data", jsonData.toString())
+        val contentIntent = PendingIntent.getActivity(this, Random().nextInt(), notificationIntent, PendingIntent.FLAG_ONE_SHOT)
+
+        // Start building notification
+        val builder: NotificationCompat.Builder
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            var channelId: String? = "default"
+            if (params["channelId"] != null && params["channelId"] !== "") {
+                channelId = params["channelId"]
+            }
+            builder = NotificationCompat.Builder(context, channelId!!)
+        } else {
+            builder = NotificationCompat.Builder(context)
+        }
+        builder.setContentIntent(contentIntent)
+        builder.setAutoCancel(true)
+        builder.priority = priority
+        builder.setContentTitle(params["title"])
+        if (params["alert"] != null) {
+            // OneSignal uses alert for the message
+            builder.setContentText(params["alert"])
+        } else {
+            builder.setContentText(params["message"])
+        }
+        builder.setTicker(params["ticker"])
+        builder.setDefaults(builderDefaults)
+        builder.setSound(defaultSoundUri)
+
+        // BigText
+        if (params["big_text"] != null && params["big_text"] !== "") {
+            val bigTextStyle = NotificationCompat.BigTextStyle()
+            bigTextStyle.bigText(params["big_text"])
+            if (params["big_text_summary"] != null && params["big_text_summary"] !== "") {
+                bigTextStyle.setSummaryText(params["big_text_summary"])
+            }
+            builder.setStyle(bigTextStyle)
+        }
+
+        if (params["color"] != null && params["color"] !== "") {
+            try {
+                val color = TiConvert.toColor(params["color"])
+                builder.color = color
+                builder.setColorized(true)
+            } catch (ex: Exception) {
+                android.util.Log.e("HCM", "Color exception: " + ex.message)
+            }
+        }
+
+        var id = 0
+        if (params != null && params["id"] !== "") {
+            // ensure that the id sent from the server is negative to prevent
+            // collision with the atomic integer
+            id = TiConvert.toInt(params["id"], 0)
+        }
+        if (id == 0) {
+            id = AtomicInteger(0).getAndIncrement()
+        }
+
+        // Send
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(id, builder.build())
+
+        return true
     }
 }
--- android/src/huawei/messaging/TitaniumHuaweiMessagingModule.kt
+++ android/src/huawei/messaging/TitaniumHuaweiMessagingModule.kt
@@ -1,5 +1,13 @@
 package huawei.messaging
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.os.Build
+import android.preference.PreferenceManager
 import com.huawei.agconnect.appmessaging.AGConnectAppMessaging
 import com.huawei.agconnect.appmessaging.AGConnectAppMessagingOnClickListener
 import com.huawei.agconnect.appmessaging.model.AppMessage
@@ -14,6 +22,7 @@ import org.appcelerator.kroll.KrollModule
 import org.appcelerator.kroll.annotations.Kroll.*
 import org.appcelerator.kroll.common.Log
 import org.appcelerator.titanium.TiApplication
+import org.json.JSONObject
 import java.util.*
 
 class ClickListener : AGConnectAppMessagingOnClickListener {
@@ -26,6 +35,7 @@ class ClickListener : AGConnectAppMessagingOnClickListener {
 class TitaniumHuaweiMessagingModule : KrollModule() {
 
     private lateinit var instance: AGConnectAppMessaging
+    private var notificationData = ""
 
     init {
         moduleInstance = this
@@ -35,6 +45,9 @@ class TitaniumHuaweiMessagingModule : KrollModule() {
     fun configure() {
         instance = AGConnectAppMessaging.getInstance()
         instance.addOnClickListener(ClickListener())
+
+        // Check for pending intents
+        parseBootIntent()
     }
 
     @method
@@ -116,13 +129,101 @@ class TitaniumHuaweiMessagingModule : KrollModule() {
         }.start()
     }
 
-    fun onMessageReceived(message: RemoteMessage) {
-        if (!hasListeners("didReceiveMessage")) { return }
+    @method
+    fun createNotificationChannel(options: KrollDict) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return
+        }
 
+        Log.d("HCM", "createNotificationChannel $options")
+
+        val context: Context = TiApplication.getInstance().applicationContext
+        val sound = options.optString("sound", "default")
+        val importance = options.optString("importance", if (sound == "silent") "low" else "default")
+        val channelId = options.optString("channelId", "default")
+        val channelName = options.optString("channelName", channelId)
+        var importanceVal = NotificationManager.IMPORTANCE_DEFAULT
+        if (importance == "low") {
+            importanceVal = NotificationManager.IMPORTANCE_LOW
+        } else if (importance == "high") {
+            importanceVal = NotificationManager.IMPORTANCE_HIGH
+        }
+        var soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val channel = NotificationChannel(channelId, channelName, importanceVal)
+        channel.enableVibration(options.optBoolean("vibrate", false))
+        channel.enableLights(options.optBoolean("lights", false))
+        channel.setShowBadge(options.optBoolean("showBadge", false))
+        if (soundUri != null) {
+            val audioAttributes = AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                    .build()
+            channel.setSound(soundUri, audioAttributes)
+        }
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    @method
+    fun deleteNotificationChannel(channelId: String) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return
+        }
+        Log.d("HCM", "deleteNotificationChannel $channelId")
+        val context: Context = TiApplication.getInstance().applicationContext
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.deleteNotificationChannel(channelId)
+    }
+
+    // clang-format off
+    @setProperty
+    @method
+    fun setForceShowInForeground(showInForeground: Boolean?) // clang-format on
+    {
+        val context: Context = TiApplication.getInstance().applicationContext
+        val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = prefs.edit()
+        editor.putBoolean("huawei.messaging.prefs.show_in_foreground", showInForeground!!)
+        editor.commit()
+    }
+
+    @getProperty
+    fun forceShowInForeground(): Boolean {
+        val context: Context = TiApplication.getInstance().applicationContext
+        val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        return prefs.getBoolean("huawei.messaging.prefs.show_in_foreground", false)
+    }
+
+    fun setNotificationData(data: String) {
+        notificationData = data
+    }
+
+    fun parseBootIntent() {
         try {
-            val data = KrollDict()
-            data["message"] = message.notification.body
-            fireEvent("didReceiveMessage", data)
+            val intent = TiApplication.getAppRootOrCurrentActivity().intent
+            val notification = intent.getStringExtra("hcm_data")
+            if (notification != null) {
+                val msg = HashMap<String, Any>()
+                msg["data"] = KrollDict(JSONObject(notification))
+                onMessageReceived(msg)
+                intent.removeExtra("hcm_data")
+            } else {
+                Log.d("HCM", "Empty notification in Intent")
+            }
+        } catch (ex: java.lang.Exception) {
+            Log.e("HCM", "parseBootIntent$ex")
+        }
+        val preferences = PreferenceManager.getDefaultSharedPreferences(TiApplication.getInstance().applicationContext)
+        preferences.edit().remove("huawei.messaging.prefs.message").commit()
+    }
+
+    fun onMessageReceived(message: HashMap<*, *>?) {
+        try {
+            if (hasListeners("didReceiveMessage")) {
+                val data = KrollDict()
+                data["message"] = KrollDict(message as MutableMap<out String, out Any>)
+                fireEvent("didReceiveMessage", data)
+            }
         } catch (e: java.lang.Exception) {
             Log.e("HCM", "Message exception: " + e.message)
         }
